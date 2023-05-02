f2168cc640b3af20e988cb2e497c99cb7db68adf
Mikaxris <73655899+Putthima@users.noreply.github.com>
2023-05-02 19:12:39 +0700

    feat: shadow and layout spacing

--- pages/leaderboard.jsx
+++ pages/leaderboard.jsx
@@ -61,36 +61,39 @@ export default function Leaderboard() {
         <style>{ style }</style>
       </Head>
       <Flex bgColor="#8B89F7" bgImage="/Img/bg_texture1.png" bgRepeat="no-repeat" bgSize="cover" w="full" h="100vh" justify="center" align="flex-end">
-        <Flex direction="column" borderWidth='5px' borderBottom="0" maxW="4xl" maxH="2xl" justify="space-between" align="center" borderColor="black" borderRadius="20px 20px 0 0" h="full" w='full' background="#ECD352">
-            <Text fontSize="6xl" py="4" color="white" style={{ WebkitTextStrokeColor: 'black', WebkitTextStrokeWidth: '2px' }} className={heyComic.className}>Leaderboard</Text>
-            <Flex direction="column" bg="white" p="2rem 2rem 0 2rem" w="80%" h="full" borderRadius="30px 30px 0 0" maxH="lg">
+        <Flex direction="column" position="relative" borderWidth='5px' borderBottom="0" maxW="4xl" maxH="2xl" align="center" borderColor="black" borderRadius="20px 20px 0 0" h="full" w='full' background="#ECD352">
+            <Flex h="full" align="center">
+                <Text fontSize="6xl" color="white" style={{ WebkitTextStrokeColor: 'black', WebkitTextStrokeWidth: '3px' }} className={heyComic.className}>Leaderboard</Text>
+            </Flex>
+            <Link href='/home'><CloseBtn className={dongle.className} right="-.5em">x</CloseBtn></Link>
+            <Flex direction="column" boxShadow="24px 20px 5px rgba(0, 0, 0, 0.25)" bg="white" p="2rem 2rem 0 2rem" w="80%" h="full" borderRadius="30px 30px 0 0" maxH="lg">
                 <Flex className={dongle.className}>
                     <Text fontSize="4xl" flex={2} textAlign="center">Name</Text>
                     <Text fontSize="4xl" flex={1}>Rank</Text>
                     <Text fontSize="4xl" flex={1}>Point</Text>
                 </Flex>
                 <Flex w="full" gap="4" direction="column" position="relative" overflowY="auto" py="2">
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555" color="#FF6839D6"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555" color="#EC7752BD"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555" color="#ECA552BD"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555" color="#ECB852BD"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
-                    <LeaderboardItem name="Pussthima" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555" color="#FF6839D6"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555" color="#EC7752BD"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555" color="#ECA552BD"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555" color="#ECB852BD"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
+                    <LeaderboardItem name="DowNy" rank="Conquerer" point="555"></LeaderboardItem>
                 </Flex>
             </Flex>
         </Flex>
