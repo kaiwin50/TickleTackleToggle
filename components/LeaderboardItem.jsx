92ecc7d4780cea1d4903bbdb9727f0b98172ad46
Mikaxris <73655899+Putthima@users.noreply.github.com>
2023-05-05 23:45:19 +0700

    feat: little responsive

--- components/LeaderboardItem.jsx
+++ components/LeaderboardItem.jsx
@@ -4,8 +4,8 @@ import { dongle } from '@/components/Font'
 const LeaderboardItem = ({ name, rank, point, color="lightgrey" }) => {
     return (
         <Flex justify="space-around" w="full" bg={color} align="center" borderRadius="20px" minH="20" className={dongle.className}>
-            <Flex flex={2} gap="4" align="center">
-                <Avatar ml="8" borderWidth="2px" borderColor="black"></Avatar>
+            <Flex flex={2} gap="4" align="center" justify={['center', 'flex-start']}>
+                <Avatar ml="8" borderWidth="2px" borderColor="black" display={['none', 'block']}></Avatar>
                 <Text fontSize="4xl">{name}</Text>
             </Flex>
             <Text fontSize="4xl" flex={1}>{rank}</Text>
